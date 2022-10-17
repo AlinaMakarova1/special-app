da5317feb67c05013cfa6ecceaa50a8a5f414bc1
Alina Makarova <alina.mikhaylova01@mail.ru>
2022-10-17 20:46:32 +0400

    chages 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int CoffeAmount = 1250;
-        int MilkAmount = 10;
+
+        int cocomilkAmound = 15;
+        int sodamilkAmound = 10;
 
         int coppuciniMilkRequred = 60;
         int coppucinoCoffeRequred = 15;
 
 
-        if (CoffeAmount >= coppucinoCoffeRequred && MilkAmount >= coppuciniMilkRequred) {
+        if (CoffeAmount >= coppucinoCoffeRequred && (cocomilkAmound || sodamilkAmound >= coppuciniMilkRequred) {
             System.out.println("Готовим капучино для вас!");
         } else {
             System.out.println("Ингредиентов недостаточно");
